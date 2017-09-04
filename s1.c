582fda4dda8051abf066d3863324fe7554841a52
sathya-123 <30828591+sathya-123@users.noreply.github.com>
2017-09-04 09:24:54 -0700

    Create s1.c

---
+++ s1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+double value;
+if(value<=0.0)
+{
+if(value==0.0)
+printf("the value is zero");
+else
+printf("the value is nagative");
+}
+else
+printf("the value is positive");
+}
+}
